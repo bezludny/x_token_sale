3f86aef7e24478dce75d7f542c397198067f7a4a
bz <bezludny@gmail.com>
2021-07-19 23:31:13 +0800

    Part 2

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -4,9 +4,16 @@ contract DappToken {
   // Constructor
   // Set amount of tokens
   // Read amount of tokens
+  string public name = 'DApp Token';
+  string public symbol = 'DAPP';
+  string public standard = 'DApp Token v1.0';
   uint256 public totalSupply;
 
-  constructor() public {
-    totalSupply = 1000000;
+
+  mapping(address => uint256) public balanceOf;
+
+  constructor(uint256 _initialSupply) public {
+    balanceOf[msg.sender] = _initialSupply;
+    totalSupply = _initialSupply;
   }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,12 +1,32 @@
 var DappToken = artifacts.require('./DappToken.sol')
 
 contract('DappToken', function(accounts) {
+  var tokenInstance;
+
+  it('inits contract with values', function(){
+    return DappToken.deployed().then(function(instance) {
+      tokenInstance = instance;
+      return tokenInstance.name();
+    }).then(function(name){
+      assert.equal(name, 'DApp Token', 'has the correct name')
+      return tokenInstance.symbol();
+    }).then(function(symbol){
+      assert.equal(symbol, 'DAPP', 'has the correct symbol')
+      return tokenInstance.standard();
+    }).then(function(standard){
+      assert.equal(standard, 'DApp Token v1.0', 'has the correct standard')
+    });
+  });
+
   it('sets the total supply', function(){
     return DappToken.deployed().then(function(instance) {
       tokenInstance = instance;
       return tokenInstance.totalSupply();
     }).then(function(totalSupply){
-      assert.equal(totalSupply.toNumber(), 100000, 'sets the total supply to 1000000');
+      assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1000000');
+      return tokenInstance.balanceOf(accounts[0]);
+    }).then(function(adminBalance) {
+      assert.equal(adminBalance.toNumber(), 1000000, 'it allocates the initial supply to the admin account')
     });
   });
 });
